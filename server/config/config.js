a982544a633118c76487c1d7c1d30da208709b34
adrianabelei <adrianabelei01@gmail.com>
2018-05-24 17:36:12 +0200

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,80 @@
+
+
+const config = {
+    production: {
+        SECRET: process.env.SECRET,
+        DATABASE: process.env.MONGODB_URI
+    },
+    default: {
+        SECRET: 'supersecretpassword123',
+        DATABASE: 'mongodb://craiglist:123456@ds163119.mlab.com:63119/craiglist',
+    }
+}
+
+exports.get = function get(env) {
+    return config[env] || config.default
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
